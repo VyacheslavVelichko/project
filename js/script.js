b9631f88aaff9117ceb3427b99316edd8d87ee69
Slava <exampl@gmail.com>
2025-02-28 19:06:58 +0200

    third

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@
 /*jshint esversion: 6 */
 "use strict";
 
-const category = 'toys';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(`https://someurl.com/${category}/5`);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const user = 'Ivan';
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt('На сколько оцените его?', '');
 
-alert(`Привет, ${user}`);
+        personalMovieDB.movies[a] = b;
+        personalMovieDB.movies[c] = d;
+
+        console.log(personalMovieDB);
